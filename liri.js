fde74f80fecc02be1091c44c4e7c13c8cbdece4b
messejessie <messejessie77@gmail.com>
2019-02-06 19:53:42 -0500

    done for now

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 //requires
 require("dotenv").config();
 var fs = require("fs");
+var keys = require("./keys.js");
 var filename = './random.txt';
 var spotify = require('node-spotify-api');
 //user arguements 
@@ -8,4 +9,7 @@ var userCommand = process.argv[2]
 var secondCommand = process.argv[3]
 //fetch spotify keys
 var spotify = new Spotify(keys.spotify);
+
+//function: 
+
 

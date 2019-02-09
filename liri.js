9a0edec37a164ffb2da110b110f2c6a7e089d902
messejessie <messejessie77@gmail.com>
2019-02-09 15:49:23 -0500

    movie is working

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@
 
 var fs = require("fs");
 var keys = require("./keys.js");
+var axios = require("axios");
 require("dotenv").config();
 var filename = './random.txt';
 var spotify = require('node-spotify-api');
@@ -16,17 +17,17 @@ var spotifyCmd = 'spotify-this-song';
 var defaultCmd = 'do-what-it-says';
 
 //fetch spotify keys
-// var spotify = new Spotify(keys.spotify);
+//var spotify = new Spotify(keys.spotify);
 
 //spotify function: 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-   
-  console.log(data); 
-  });
-  
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//     if (err) {
+//       return console.log('Error occurred: ' + err);
+//     }
+
+//   console.log(data); 
+//   });
+
 // spotify pending 
 
 //commands
@@ -49,3 +50,20 @@ function getCmd(userCmd) {
 
 getCmd(userCmd);
 
+axios.get(`http://www.omdbapi.com/?t=${secondCommand}&y=&plot=short&apikey=2973baab`).then(
+    function (response) {
+        let movie = response.data;
+        // console.log(movie);
+        let movieResponse = [
+            'Title:' + movie.Title,
+            'Year: ' + movie.Year,
+            'IMDB Rating: ' + movie.imdbRating,
+            'Rotten Tomatoes: ' + movie.Ratings[1].Value,
+            'Country: ' + movie.Country,
+            'Language: ' + movie.Language,
+            'Plot: ' + movie.Plot,
+            'Actors: ' + movie.Actors,
+        ];
+        console.log(movieResponse);
+    }
+);
